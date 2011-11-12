929d35027c04f31bc8f5a6f679caee9dced34ef3
Greg Terrono <gterrono@seas.upenn.edu>
2011-11-12 01:29:59 -0500

    Fixed department.html to show courses

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse
+from api import pcr
+from wrapper import *
 
 def index(request):
   return render_to_response('index.html')
 
-def department(request):
-  return render_to_response('department.html')
+def department(request,dept):
+  d=Department(pcr(''.join(('dept/',dept))))
+  return render_to_response('department.html', {'c':d.coursehistories,})
